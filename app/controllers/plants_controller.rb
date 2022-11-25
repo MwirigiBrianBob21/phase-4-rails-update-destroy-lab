631ada5ae562d7ad7edf2e3af6ebd4f513bbeb9f
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-11-25 03:26:41 +0300

    Post

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
 
+# rescue from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +20,41 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    # plant.update(plant_params)
+     plant.update(is_in_stock: bird.is_in_stock = true)
+
+    render json: plant
+  # rescue ActiveRecord::RecordNotFound
+  #   render_not_found_response
+  end
+
+
+  # PATCH /birds/:id/like
+  # def add_in_stock
+  #   plant = find_plant
+  #   plant.update(is_in_stock: bird.is_in_stock = true)
+  #   render json: plant
+  # end
+
+   # DELETE /birds/:id
+   def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  # def render_not_found_response
+  #   render json: { error: "Plant not found" }, status: :not_found
+  # end
 end
